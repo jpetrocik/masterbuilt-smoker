c9a823b7798b1ee1cd3217761d3b99fa618b905a
John Petrocik <john@petrocik.net>
2023-02-26 17:44:20 -0800

    Added OTA

--- thermistor.ino
+++ thermistor.ino
@@ -12,6 +12,7 @@
 #include <ESPAsyncWebServer.h>
 #include <Arduino_JSON.h>
 #include <PID_v1.h>
+#include <ArduinoOTA.h>
 
 #define SSID "PNET"
 #define PASSWORD "5626278472"
@@ -101,6 +102,8 @@ void setup(void) {
 
   initWebSocket();
 
+  initSetup();
+  
   server.begin();
 
   pinMode(HEAT_PIN, OUTPUT);
@@ -109,6 +112,8 @@ void setup(void) {
 }
 
 void loop(void) {
+  ArduinoOTA.handle();
+
   if (abortError) {
     digitalWrite(HEAT_PIN, LOW);
     return;
@@ -327,4 +332,35 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
 void initWebSocket() {
   ws.onEvent(onEvent);
   server.addHandler(&ws);
+}
+
+void initSetup() { 
+  Serial.println("Enabling OTA Updates");
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  ArduinoOTA.setHostname("smoker");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123");
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("OTA Update Start....");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nOTA Update Finished");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
 }
